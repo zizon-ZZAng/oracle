faf52dd4ef1ebfb38c69205f22770b48f680c56d
탁승은 <xkrtmddms@gmail.com>
2023-02-21 14:53:23 +0900

    20230221_SQL

---
+++ src/connection/MyBatisContext.java
@@ -0,0 +1,24 @@
+package connection;
+
+import org.apache.ibatis.session.SqlSession;
+
+
+// 외부에서 쓸 땐 MyBatisContext.getSqlSession();
+public class MyBatisContext {
+	
+	public static SqlSession getSqlSession() {
+		try {
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	
+}
