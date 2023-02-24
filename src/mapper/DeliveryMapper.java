8108c6fe530ae6d4310e6c47a850052e6bc73cb4
탁승은 <xkrtmddms@gmail.com>
2023-02-24 17:02:29 +0900

    20230224

--- src/mapper/DeliveryMapper.java
+++ src/mapper/DeliveryMapper.java
@@ -1,8 +1,12 @@
 package mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import dto.Delivery;
 
@@ -15,5 +19,34 @@ public interface DeliveryMapper {
 	public int insertDelivery(@Param("delivery") Delivery delivery);
 	
 	
+	//메뉴별 전체 주문 수량 및 전체 금액
+	@Select({"SELECT menuno, name, sum(cnt) totalcnt, price * cnt totalprice  FROM deliveryinfoview df GROUP BY menuno, name, price, cnt"})
+	public List<Map<String, Object>> deliveryMenuView();
+	
+//	고객별 전체주문 횟수 및 전체 금액 
+	@Select({" SELECT email, COUNT(*) ordercount, sum(price * cnt) totalprice  FROM deliveryinfoview df GROUP BY email "})
+	public List<Map<String, Object>> cutomerOrderView();
+	
+	
+	//주문 시간대별 주문횟수 및 전체 금액
+	@Select({"SELECT TO_CHAR(regdate,'HH24:MI')  , COUNT(*) ordercount, sum(price * cnt)totalprice FROM deliveryinfoview GROUP BY TO_CHAR(regdate,'HH24:MI') "})
+	public List<Map<String, Object>> hourOrderView();
+	
+	//식당연락처가 전달되면 해당 식당의 메뉴별 전체 주문 수량
+	@Select({" SELECT name, SUM(cnt) ordercnt FROM deliveryinfoview WHERE resphone=#{resphone} GROUP BY name, resphone "})
+	public List<Map<String, Object>> restaurantPhoneMenuView(String phone);
+	
+	
+	//배달 시간대별 배달 횟수
+	@Select({" SELECT TO_CHAR(deliverydate, 'HH24') deliveryHour , COUNT(*) deliverycount FROM deliveryinfoview GROUP BY TO_CHAR(deliverydate, 'HH24') "})
+	public List<Map<String, Object>> deliveryCountView();
+	
+	//배달자별 배달건수
+	@Select({" SELECT ridername, COUNT(*) deliverycnt FROM deliveryinfoview  GROUP BY ridername "})
+	public List<Map<String, Object>> deliveryCountView2();
+
+	//요일별 주문횟수
+	@Select({" SELECT TO_CHAR(regdate, 'DAY') day, COUNT(*) count FROM deliveryinfoview GROUP BY TO_CHAR(regdate, 'DAY') "})
+	public List<Map<String, Object>> dayOrderView();
 	
 }
--- src/test/DeliveryTest.java
+++ src/test/DeliveryTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import connection.MyBatisContext;
@@ -22,5 +25,78 @@ class DeliveryTest {
 		System.out.println(ret);
 		
 	}
+	
+	//메뉴별 전체 주문 수량 및 전체 금액
+	@Test
+	void deliveryMenuView() {
+		List<Map<String, Object>> list = mapper.deliveryMenuView();
+		for(Map<String, Object> map : list) {
+//			System.out.print(map.get("MENUNO")+" ");
+//			System.out.print(map.get("NAME")+" ");
+//			System.out.print(map.get("TOTALCNT")+" ");
+//			System.out.println(map.get("TOTALPRICE")+" ");
+			System.out.println(map.toString());
+		}
+	}
+	
+	//고객별 전체주문 횟수 및 전체 금액 
+	@Test
+	void customerOrderView() {
+		List<Map<String, Object>> list = mapper.cutomerOrderView();
+		for(Map<String, Object> map : list) {
+			System.out.println(map.toString());
+		}
+	}
+	
+	//주문 시간대별 주문횟수 및 전체 금액
+	@Test
+	void hourOrderViewView() {
+		List<Map<String, Object>> list = mapper.hourOrderView();
+		for(Map<String, Object> map : list) {
+			System.out.println(map.toString());
+		}
+	}
+	
+	//배달 시간대별 배달횟수
+	
+	//식당연락처가 전달되면 해당 식당의 메뉴별 전체 주문 수량
+	@Test
+	void restaurantPhoneMenu() {
+		List<Map<String, Object>> list = mapper.restaurantPhoneMenuView("051-000-0000");
+		for(Map<String, Object> map : list) {
+			System.out.println(map.toString());
+		}
+	}
+	
+	//배달 시간대별 배달 횟수
+	@Test
+	void deliveryCount() {
+		List<Map<String, Object>> list = mapper.deliveryCountView();
+		for(Map<String, Object> map : list) {
+			System.out.println(map.toString());
+		}
+	}
+	
+	//배달자별 배달건수
+	@Test
+	void deliveryCount2() {
+		List<Map<String, Object>> list = mapper.deliveryCountView2();
+		for(Map<String, Object> map : list) {
+			System.out.println(map.toString());
+		}
+	}
+	
+	//요일별 주문횟수
+	@Test
+	void dayOrder() {
+		List<Map<String, Object>> list = mapper.dayOrderView();
+		for(Map<String, Object> map : list) {
+			System.out.println(map.toString());
+		}
+	}
+	
+	
+	
+	
 
 }
