3c25376ec95f8b8f9ab3980e160b3685cddc0445
탁승은 <xkrtmddms@gmail.com>
2023-03-03 15:20:03 +0900

    20230303

---
+++ src/dto/Member.java
@@ -0,0 +1,18 @@
+package dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+
+@Data
+public class Member {
+	private String id;
+	private String name;
+	private String gender;
+	private String password;
+	private String address;
+	private int chk;
+	private Date regdate;
+
+}
