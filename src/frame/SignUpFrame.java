9d590bd75b926d365938ee03a989adcfbd545fa6
yeofriendship <yeofriendship@naver.com>
2023-03-06 17:21:22 +0900

    230306

--- src/frame/SignUpFrame.java
+++ src/frame/SignUpFrame.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 import dto.Member;
 import service.MemberService;
 import service.MemberServiceImpl;
+import javax.swing.JToggleButton;
 
 public class SignUpFrame extends JFrame {
 	MemberService mService = new MemberServiceImpl();
@@ -28,48 +29,48 @@ public class SignUpFrame extends JFrame {
 		getContentPane().setLayout(null);
 
 		JLabel lblNewLabel_1 = new JLabel("아이디");
-		lblNewLabel_1.setBounds(71, 66, 57, 15);
+		lblNewLabel_1.setBounds(98, 68, 57, 15);
 		getContentPane().add(lblNewLabel_1);
 
 		JLabel lblNewLabel_2 = new JLabel("비밀번호");
-		lblNewLabel_2.setBounds(71, 105, 57, 15);
+		lblNewLabel_2.setBounds(98, 107, 57, 15);
 		getContentPane().add(lblNewLabel_2);
 
 		JLabel lblNewLabel_3 = new JLabel("주소");
-		lblNewLabel_3.setBounds(71, 141, 57, 15);
+		lblNewLabel_3.setBounds(98, 143, 57, 15);
 		getContentPane().add(lblNewLabel_3);
 
 		JLabel lblNewLabel_4 = new JLabel("성별");
-		lblNewLabel_4.setBounds(71, 178, 57, 15);
+		lblNewLabel_4.setBounds(98, 180, 57, 15);
 		getContentPane().add(lblNewLabel_4);
 
 		JLabel lblNewLabel_5 = new JLabel("이름");
-		lblNewLabel_5.setBounds(71, 28, 57, 15);
+		lblNewLabel_5.setBounds(98, 30, 57, 15);
 		getContentPane().add(lblNewLabel_5);
 
 		textField = new JTextField();
-		textField.setBounds(134, 25, 116, 21);
+		textField.setBounds(161, 27, 116, 21);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 
 		textField_1 = new JTextField();
-		textField_1.setBounds(134, 63, 116, 21);
+		textField_1.setBounds(161, 65, 116, 21);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 
 		textField_2 = new JTextField();
-		textField_2.setBounds(134, 102, 116, 21);
+		textField_2.setBounds(161, 104, 116, 21);
 		getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(134, 138, 116, 21);
+		textField_3.setBounds(161, 140, 116, 21);
 		getContentPane().add(textField_3);
 
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "F", "M" }));
-		comboBox_1.setBounds(134, 174, 57, 23);
+		comboBox_1.setBounds(161, 176, 57, 23);
 		getContentPane().add(comboBox_1);
 
 		JButton btnNewButton = new JButton("회원가입");
@@ -121,13 +122,22 @@ public class SignUpFrame extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton.setBounds(234, 209, 97, 23);
+		btnNewButton.setBounds(140, 217, 97, 23);
 		getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("←");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new HomeFrame();
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(0, 0, 45, 23);
+		getContentPane().add(btnNewButton_1);
 
 		this.setSize(400, 300);
 		this.setLocationRelativeTo(null); // 화면 중앙에 오게 해줌
 
 		this.setVisible(true);
 	}
-
 }
