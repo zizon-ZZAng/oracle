c6586c484a83080e9094c19f94b6dd8959b25d29
탁승은 <xkrtmddms@gmail.com>
2023-03-07 01:48:02 +0900

    Update UnRegisterFrame.java

--- src/frame/UnRegisterFrame.java
+++ src/frame/UnRegisterFrame.java
@@ -5,58 +5,97 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import dto.Member;
 import service.MemberService;
 import service.MemberServiceImpl;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.security.MessageDigest;
 import java.awt.event.ActionEvent;
 
 public class UnRegisterFrame extends JFrame {
-	
+
 	MemberService mService = new MemberServiceImpl();
-	
+
 	private JTextField textField;
 	private JTextField textField_1;
-	
+
 	public UnRegisterFrame() {
 		setTitle("회원탈퇴");
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("아이디");
 		lblNewLabel.setBounds(100, 68, 57, 15);
 		getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("비밀번호");
 		lblNewLabel_1.setBounds(100, 120, 57, 15);
 		getContentPane().add(lblNewLabel_1);
-		
+
 		textField = new JTextField();
 		textField.setBounds(191, 65, 116, 21);
 		getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(191, 117, 116, 21);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JButton btnNewButton = new JButton("회원탈퇴");
 		btnNewButton.addActionListener(new ActionListener() {
+
+			// 비밀번호 암호화
+			public String hashPW(String pw, String id) {
+				try {
+
+					MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+					md.update((pw + id).getBytes());
+
+					byte[] pwdSalt = md.digest();
+
+					StringBuffer sb = new StringBuffer();
+					for (byte b : pwdSalt) {
+						sb.append(String.format("%02x", b));
+					}
+
+					String result = sb.toString();
+					return result;
+
+				} catch (Exception e) {
+					e.printStackTrace();
+					return null;
+				}
+
+			}
+
 			public void actionPerformed(ActionEvent e) {
 				
+
+				String id = textField.getText();
+				String pw = textField_1.getText();
+
+				String hash = this.hashPW(pw, id);
+
+				Member member = new Member();
 				
+				member.setId(id);
+				member.setPassword(hash);
+
+				mService.unMember(member);
+
 				JOptionPane.showMessageDialog(null, "회원 탈퇴 완료");
 				
+
 				new HomeFrame();
 				dispose();
-				
+
 			}
 		});
 		btnNewButton.setBounds(167, 192, 97, 23);
 		getContentPane().add(btnNewButton);
 	}
-	
-	
 
 }
