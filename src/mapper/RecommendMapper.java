bd803a2a57808a9f755cc6ddd19f6c191d416a15
0hzL <arin980921@naver.com>
2023-03-06 15:49:02 +0900

    1

--- src/mapper/RecommendMapper.java
+++ src/mapper/RecommendMapper.java
@@ -1,6 +1,8 @@
 package mapper;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import dto.Recommend;
 
@@ -10,8 +12,20 @@ public interface RecommendMapper {
 	//추천목록 넣기
 	@Insert({
 		" INSERT INTO recommend1(no,id,clno,code) ", 
-	    " VALUES(SEQ_RECOMMEND_NO1.NEXTVAL,'kim',10001, 10) "
+	    " VALUES(SEQ_RECOMMEND_NO1.NEXTVAL,#{id},#{clno}, #{code}) "
 	    
 	})
 	public int insertRecommend(Recommend r);
+	
+	@Select({
+		
+	})
+	public int selectRecommend(Recommend r);
+	
+	@Update({
+		" UPDATE member1 SET name=#{name} , password=#{password} ,address=#{address} ",
+				" WHERE id=#{id} "
+	})
+	public int updateRecommend(Recommend r);
+	
 }
