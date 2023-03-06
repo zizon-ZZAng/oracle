886695fe1279f1e4eca75c95443558955aa16181
LEEYEEJIN <leyee0202@naver.com>
2023-03-06 12:27:15 +0900

    1

---
+++ src/service/ClothesService.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import connection.MyBatisContext;
+import dto.Clothes;
+import mapper.ClothesMapper;
+
+public interface ClothesService {
+
+	final ClothesMapper mapper = MyBatisContext.getSqlSession().getMapper(ClothesMapper.class);
+	
+	
+	// 옷 등록
+	public int insertClothes(Clothes c);
+	
+	// 옷 수정
+	public int UpdateClothesOne(@Param("obj") Clothes obj);
+	
+	// 옷 삭제
+	public int deleteClothes(@Param("obj") Clothes obj);
+	
+	// 옷 조회
+	public List<Clothes> selectClothesList();
+	
+	// 문자를 포함한 옷 검색
+	public List<Clothes> clothesLikeList( @Param("map") Map<String, String> map);
+	
+	
+}
