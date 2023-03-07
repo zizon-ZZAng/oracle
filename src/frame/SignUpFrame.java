ddf82a18d82bde4a01ab124120dce2f408e66317
LEEYEEJIN <leyee0202@naver.com>
2023-03-07 14:41:55 +0900

    1

--- src/frame/SignUpFrame.java
+++ src/frame/SignUpFrame.java
@@ -1,15 +1,65 @@
 package frame;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JToolBar;
+import javax.swing.JButton;
 
 public class SignUpFrame extends JFrame{
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 	public SignUpFrame() {
-		setTitle("로그인");
 		getContentPane().setLayout(null);
 		
+		JLabel lblNewLabel = new JLabel("아이디");
+		lblNewLabel.setBounds(46, 40, 57, 15);
+		getContentPane().add(lblNewLabel);
 		
+		JLabel lblNewLabel_1 = new JLabel("이름");
+		lblNewLabel_1.setBounds(46, 84, 57, 15);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("비밀번호");
+		lblNewLabel_2.setBounds(46, 129, 57, 15);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("주소");
+		lblNewLabel_3.setBounds(46, 169, 57, 15);
+		getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("성별");
+		lblNewLabel_4.setBounds(46, 208, 57, 15);
+		getContentPane().add(lblNewLabel_4);
+		
+		textField = new JTextField();
+		textField.setBounds(155, 37, 116, 21);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(155, 81, 116, 21);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(155, 126, 116, 21);
+		getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(155, 166, 116, 21);
+		getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(155, 204, 116, 23);
+		getContentPane().add(comboBox);
+		
+		JButton btnNewButton = new JButton("완료");
+		btnNewButton.setBounds(118, 276, 97, 23);
+		getContentPane().add(btnNewButton);
 	}
 }
