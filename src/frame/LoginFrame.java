d94106b2611e274af7099de73460ae7173e6914e
0hzL <arin980921@naver.com>
2023-03-07 16:27:59 +0900

    1

--- src/frame/LoginFrame.java
+++ src/frame/LoginFrame.java
@@ -12,67 +12,69 @@ import javax.swing.JTextField;
 import service.MemberService;
 import service.MemberServiceImpl;
 
-public class LoginFrame extends JFrame implements ActionListener{
+public class LoginFrame extends JFrame implements ActionListener {
 	private JTextField textField_id;
 	private JTextField textField_pw;
+
 	public LoginFrame() {
+		setTitle("로그인");
 		getContentPane().setLayout(null);
-		
+
 		textField_id = new JTextField();
-		textField_id.setBounds(201, 52, 116, 21);
+		textField_id.setBounds(165, 52, 116, 21);
 		getContentPane().add(textField_id);
 		textField_id.setColumns(10);
-		
+
 		textField_pw = new JTextField();
-		textField_pw.setBounds(201, 100, 116, 21);
+		textField_pw.setBounds(165, 100, 116, 21);
 		getContentPane().add(textField_pw);
 		textField_pw.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel("아이디");
-		lblNewLabel.setBounds(96, 55, 57, 15);
+		lblNewLabel.setBounds(60, 55, 57, 15);
 		getContentPane().add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("비밀번호");
-		lblNewLabel_1.setBounds(96, 103, 57, 15);
+		lblNewLabel_1.setBounds(60, 103, 57, 15);
 		getContentPane().add(lblNewLabel_1);
-		
+
 		JButton loginButton = new JButton("로그인");
 		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		loginButton.setBounds(91, 184, 97, 23);
+		loginButton.setBounds(55, 184, 97, 23);
 		getContentPane().add(loginButton);
-		
+
 		JButton signUpButton = new JButton("회원가입");
-		signUpButton.setBounds(249, 184, 97, 23);
+		signUpButton.setBounds(213, 184, 97, 23);
 		getContentPane().add(signUpButton);
-		
-		this.setSize(400, 400);	// 사이즈 정하기
+
+		this.setSize(400, 400); // 사이즈 정하기
 		this.setVisible(true);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-if(e.getActionCommand().equals("등록하기")) {
-			
+
+		if (e.getActionCommand().equals("등록하기")) {
+
 			// 아이디, 비밀번호 정보 가져오기
 			String id = textField_id.getText();
 			String pw = textField_pw.getText();
-						
+
 			MemberService s = new MemberServiceImpl();
-							
+
 			int ret = s.loginMember(id, pw);
-			
+
 			System.out.println(ret);
-			if(ret ==1) {
+			if (ret == 1) {
 				JOptionPane.showMessageDialog(null, "로그인 되었습니다.");
-				System.exit(0);	// 화면 종료
-			}
-			else {
+				System.exit(0); // 화면 종료
+			} else {
 				JOptionPane.showMessageDialog(null, "아이디,비번이 잘못되었습니다.");
 			}
-		
+
+		}
 	}
 }
