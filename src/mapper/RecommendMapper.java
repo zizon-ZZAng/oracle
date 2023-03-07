6c36524b380bbcaee715a067df1570e5cc971461
탁승은 <xkrtmddms@gmail.com>
2023-03-07 12:21:48 +0900

    Update RecommendMapper.java

--- src/mapper/RecommendMapper.java
+++ src/mapper/RecommendMapper.java
@@ -4,5 +4,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RecommendMapper {
+	
+	//옷추천
+	@Select("SELECT * FROM view_clovws2 where locname=#{locname} AND wdate=#{wdate} AND (catetype='상의' OR catetype='하의' OR catetype='신발')")
+	public 
 
 }
