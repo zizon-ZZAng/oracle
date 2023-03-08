187aa6058d4710abc273f7caa9eadf77eaf49712
khj <kanokaska45@mail.com>
2023-03-08 11:37:45 +0900

    1

--- src/frame/WeatherFrame.java
+++ src/frame/WeatherFrame.java
@@ -1,111 +1,135 @@
 package frame;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.print.Book;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+import dto.Location;
+import dto.Member;
 import dto.Weather;
-import service.WeatherService;
 import service.WeatherServiceImpl;
 
-public class WeatherFrame extends JDialog implements ActionListener {
+
+public class WeatherFrame extends JFrame {
 	public WeatherFrame() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{46, 52, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
+		getContentPane().setLayout(null);
 		
-		
-		
-		JLabel lblNewLabel = new JLabel("지역");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel.gridx = 2;
-		gbc_lblNewLabel.gridy = 1;
-		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
-		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 3;
-		gbc_comboBox.gridy = 1;
-		getContentPane().add(comboBox, gbc_comboBox);
-		
-		
-		
-		JLabel lblNewLabel_1 = new JLabel("날짜");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_1.gridx = 2;
-		gbc_lblNewLabel_1.gridy = 2;
-		getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1.gridx = 3;
-		gbc_comboBox_1.gridy = 2;
-		getContentPane().add(comboBox_1, gbc_comboBox_1);
-		
-		
-		
-		JLabel lblNewLabel_1_1 = new JLabel("시간");
-		GridBagConstraints gbc_lblNewLabel_1_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1_1.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_1_1.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_1_1.gridx = 2;
-		gbc_lblNewLabel_1_1.gridy = 3;
-		getContentPane().add(lblNewLabel_1_1, gbc_lblNewLabel_1_1);
-		
-		JComboBox comboBox_1_1 = new JComboBox();
-		GridBagConstraints gbc_comboBox_1_1 = new GridBagConstraints();
-		gbc_comboBox_1_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1_1.gridx = 3;
-		gbc_comboBox_1_1.gridy = 3;
-		getContentPane().add(comboBox_1_1, gbc_comboBox_1_1);
-	}
 
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.println(e.getActionCommand());
 		
-		if(e.getActionCommand().equals("지역<")) {
-			WeatherService s = new WeatherServiceImpl();
-			String a = new s.weatherSelect(null);
-			JComboBox combo = new JComboBox();
-			combo.addItem(a);
-			
-		}
-			
-			String title = comboBox.getText();
-			String author = comboBox_1.getText();
-			String price = comboBox_1_1.getText();
-		
-					
-			Weather book = new Book();
-			book.setTitle(title);
-			book.setAuthor(author);
-			book.setPrice( Long.parseLong(price));
-			book.setCate(cate.charAt(0));
-			// 4개의 항목의 값을 가져와서 Book타입 DB에 저장하는 소스 구현
-			
-			int ret = bookDB.insertBook(book);
-		
-	}
+		JButton myPageButton = new JButton("마이페이지");
+		myPageButton.setBounds(22, 22, 97, 23);
 
+		getContentPane().add(myPageButton);
+		
+		
+		JLabel lblNewLabel_loca = new JLabel("지역");
+		lblNewLabel_loca.setBounds(32, 68, 30, 15);
+		getContentPane().add(lblNewLabel_loca);
+
+		JComboBox comboBox_loca = new JComboBox();
+		comboBox_loca.setModel(new DefaultComboBoxModel(new String[] {"서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도"}));
+		comboBox_loca.setBounds(62, 64, 87, 23);
+		getContentPane().add(comboBox_loca);
+		
+		JLabel lblNewLabel_date = new JLabel("날짜");
+		lblNewLabel_date.setBounds(180, 68, 30, 15);
+		getContentPane().add(lblNewLabel_date);
+
+		JComboBox comboBox_date = new JComboBox();
+		comboBox_date.setModel(new DefaultComboBoxModel(new String[] {"2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-07"}));
+		comboBox_date.setBounds(210, 64, 87, 23);
+		getContentPane().add(comboBox_date);
+		
+		JLabel lblNewLabel_time = new JLabel("시간");
+		lblNewLabel_time.setBounds(300, 68, 30, 15);
+		getContentPane().add(lblNewLabel_time);
+		
+		JComboBox comboBox_time = new JComboBox();
+		comboBox_time.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Weather w = new Weather();
+				w.set
+				
+			m.setId(textField_id.getText());
+				m.setName(textField_name.getText()); 
+				m.setPassword(passwordField.getText());
+				m.setAddress(comboBox_Add.getSelectedItem().toString());
+				m.setSex(comboBox_Sex.getSelectedItem().toString());
+				
+				int ret = s.insertMember(m);
+
+		        }
+			
+		});
+		comboBox_time.setModel(new DefaultComboBoxModel(new String[] {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00"}));
+		comboBox_time.setBounds(330, 64, 50, 23);
+		getContentPane().add(comboBox_time);
+		
+
+		JButton recButton = new JButton("옷 추천");
+
+		recButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new RecommendFrame();
+				dispose();
+			}
+		});
+		recButton.setBounds(153, 257, 97, 23);
+		getContentPane().add(recButton);
+		
+		this.setSize(400, 400);	// 사이즈 정하기
+
+		recButton.setBounds(162, 298, 97, 23);
+		getContentPane().add(recButton);
+	
+		//
+		
+		recButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+			
+			//네 ?
+		});
+		
+		
+		
+		
+		this.setSize(434, 400);	// 사이즈 정하기
+
+		this.setVisible(true);
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
