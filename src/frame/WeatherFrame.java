1b3619bbf9d50e7c6d60202ea1c1accd009753e3
khj <kanokaska45@mail.com>
2023-03-08 11:24:17 +0900

    1

--- src/frame/WeatherFrame.java
+++ src/frame/WeatherFrame.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import dto.Location;
+import service.WeatherServiceImpl;
 
 public class WeatherFrame extends JFrame {
 	public WeatherFrame() {
@@ -21,38 +22,56 @@ public class WeatherFrame extends JFrame {
 		getContentPane().add(myPageButton);
 		
 		
-		JLabel lblNewLabel = new JLabel("지역");
-		lblNewLabel.setBounds(32, 68, 30, 15);
-		getContentPane().add(lblNewLabel);
+		JLabel lblNewLabel_loca = new JLabel("지역");
+		lblNewLabel_loca.setBounds(32, 68, 30, 15);
+		getContentPane().add(lblNewLabel_loca);
 
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도"}));
-		comboBox.setBounds(62, 64, 87, 23);
-		getContentPane().add(comboBox);
+		JComboBox comboBox_loca = new JComboBox();
+		comboBox_loca.setModel(new DefaultComboBoxModel(new String[] {"서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도"}));
+		comboBox_loca.setBounds(62, 64, 87, 23);
+		getContentPane().add(comboBox_loca);
 		
-		JLabel lblNewLabel_1 = new JLabel("날짜");
-		lblNewLabel_1.setBounds(180, 68, 30, 15);
-		getContentPane().add(lblNewLabel_1);
+		JLabel lblNewLabel_date = new JLabel("날짜");
+		lblNewLabel_date.setBounds(180, 68, 30, 15);
+		getContentPane().add(lblNewLabel_date);
 
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-07"}));
-		comboBox_1.setBounds(210, 64, 87, 23);
-		getContentPane().add(comboBox_1);
+		JComboBox comboBox_date = new JComboBox();
+		comboBox_date.setModel(new DefaultComboBoxModel(new String[] {"2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-06", "2023-03-07"}));
+		comboBox_date.setBounds(210, 64, 87, 23);
+		getContentPane().add(comboBox_date);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("시간");
-		lblNewLabel_1_1.setBounds(300, 68, 30, 15);
-		getContentPane().add(lblNewLabel_1_1);
+		JLabel lblNewLabel_time = new JLabel("시간");
+		lblNewLabel_time.setBounds(300, 68, 30, 15);
+		getContentPane().add(lblNewLabel_time);
 		
-		JComboBox comboBox_1_1 = new JComboBox();
-		comboBox_1_1.setModel(new DefaultComboBoxModel(new String[] {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00"}));
-		comboBox_1_1.setBounds(330, 64, 50, 23);
-		getContentPane().add(comboBox_1_1);
+		JComboBox comboBox_time = new JComboBox();
+		comboBox_time.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+//				 if (e.getSource() == comboBox_time) {
+//		                System.out.println("CENTER 클릭됨");
+//		            } else if (e.getSource() ==  ) {
+//		                System.out.println("NORTH 클릭됨");
+//		            } else if (e.getSource() == btnSouth) {
+//		                System.out.println("South 클릭됨");
+//		            } else if (e.getSource() == btnEast) {
+//		                System.out.println("East 클릭됨");
+//		            } else if (e.getSource() == btnWest) {
+//		                System.out.println("West 클릭됨");
+//		            }
+		        }
+			
+		});
+		comboBox_time.setModel(new DefaultComboBoxModel(new String[] {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "00"}));
+		comboBox_time.setBounds(330, 64, 50, 23);
+		getContentPane().add(comboBox_time);
 		
 
 		JButton recButton = new JButton("옷 추천");
 		recButton.setBounds(162, 298, 97, 23);
 		getContentPane().add(recButton);
-		
+	
+		//
 		
 		recButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
