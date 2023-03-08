8d1480ebf34055107c78c9333e5f171cec9b1d89
탁승은 <xkrtmddms@gmail.com>
2023-03-08 09:15:15 +0900

    Update RecommendMapper.java

--- src/mapper/RecommendMapper.java
+++ src/mapper/RecommendMapper.java
@@ -11,10 +11,12 @@ import dto.Recommend;
 @Mapper
 public interface RecommendMapper {
 	
-	//옷추천
+	//옷추천 
 	@Select("SELECT * FROM view_mcv WHERE wdate=#{wdate} AND (catetype='상의' OR catetype='하의' OR catetype='신발') AND id=#{id}")
-	
 	public List<Map<String, Object>> clothesRecommend(Map<String, Object> map);
+	
+	//옷추천 - 상의
+	
 	
 
 }
