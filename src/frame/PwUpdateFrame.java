2d9d8e18bdf163e2df4cdfe5078aa4db20868be6
yeofriendship <yeofriendship@naver.com>
2023-03-08 12:29:43 +0900

    230308

--- src/frame/PwUpdateFrame.java
+++ src/frame/PwUpdateFrame.java
@@ -14,14 +14,14 @@ import common.Config;
 import dto.Member;
 import service.MemberService;
 import service.MemberServiceImpl;
+import javax.swing.JPasswordField;
 
 public class PwUpdateFrame extends JFrame {
 
 	MemberService mService = new MemberServiceImpl();
-
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JPasswordField passwordField;
+	private JPasswordField passwordField_1;
+	private JPasswordField passwordField_2;
 
 	public PwUpdateFrame() {
 		setTitle("비밀번호 변경");
@@ -40,23 +40,20 @@ public class PwUpdateFrame extends JFrame {
 		getContentPane().add(lblNewLabel_2);
 		
 		// 현재 비밀번호
-		textField = new JTextField();
-		textField.setBounds(214, 49, 116, 21);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-
-		// 새 비밀번호
-		textField_1 = new JTextField();
-		textField_1.setBounds(214, 86, 116, 21);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-
-		// 새 비밀번호(확인)
-		textField_2 = new JTextField();
-		textField_2.setBounds(214, 126, 116, 21);
-		getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		passwordField = new JPasswordField();
+		passwordField.setBounds(214, 49, 116, 21);
+		getContentPane().add(passwordField);
 		
+		// 새 비밀번호
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(214, 86, 116, 21);
+		getContentPane().add(passwordField_1);
+		
+		// 새 비밀번호 (확인)
+		passwordField_2 = new JPasswordField();
+		passwordField_2.setBounds(214, 126, 116, 21);
+		getContentPane().add(passwordField_2);
+
 		JButton btnNewButton = new JButton("변경하기");
 		btnNewButton.addActionListener(new ActionListener() {
 			// 비밀번호 암호화
@@ -83,40 +80,37 @@ public class PwUpdateFrame extends JFrame {
 				}
 
 			}
-			
+
 			public void actionPerformed(ActionEvent e) {
 				String id = Config.obj.getId();
-				String pw = textField.getText();
-				String newpw = textField_1.getText();
-				String newpwchk = textField_2.getText();
-				
+				String pw = passwordField.getText();
+				String newpw = passwordField_1.getText();
+				String newpwchk = passwordField_2.getText();
+
 				String hash = this.hashPW(pw, id);
 				String newhash = this.hashPW(newpw, id);
 				String newhashchk = this.hashPW(newpwchk, id);
-				
+
 				Member member = new Member();
 				member.setId(id);
 				member.setPassword(hash);
 				member.setNewpw(newhash);
-				
-				int ret = mService.updatePWMember(member);
-				
-				if (ret == 0) {
-					JOptionPane.showMessageDialog(null, "성공", "비밀번호 변경", JOptionPane.PLAIN_MESSAGE);
+
+				if (!hash.equals(Config.obj.getPassword())) {
+					JOptionPane.showMessageDialog(null, "현재 비밀번호가 틀렸습니다", "비밀번호 변경", JOptionPane.ERROR_MESSAGE);
+				}
+				else if (hash.equals(newhash)) {
+					JOptionPane.showMessageDialog(null, "새 비밀번호를 현재 비밀번호와 다르게 입력해주세요", "비밀번호 변경", JOptionPane.ERROR_MESSAGE);
+				}
+				else if (!newhash.equals(newhashchk)) {
+					JOptionPane.showMessageDialog(null, "새 비밀번호를 확인해주세요", "비밀번호 변경", JOptionPane.ERROR_MESSAGE);
 				}
 				else {
-					if (hash != Config.obj.getPassword()) {
-						JOptionPane.showMessageDialog(null, "현재 비밀번호가 틀렸습니다", "비밀번호 변경", JOptionPane.ERROR_MESSAGE);
-					}
-//					else if (hash == newhash) {
-//						JOptionPane.showMessageDialog(null, "새 비밀번호를 현재 비밀번호와 다르게 입력해주세요", "비밀번호 변경", JOptionPane.ERROR_MESSAGE);
-//					}
-					else if (newhash != newhashchk) {
-						JOptionPane.showMessageDialog(null, "새 비밀번호를 확인해주세요", "비밀번호 변경", JOptionPane.ERROR_MESSAGE);
-					}
+					mService.updatePWMember(member);
+					JOptionPane.showMessageDialog(null, "성공", "비밀번호 변경", JOptionPane.PLAIN_MESSAGE);
 				}
-				
-			}	
+
+			}
 		});
 		btnNewButton.setBounds(83, 183, 97, 23);
 		getContentPane().add(btnNewButton);
@@ -130,7 +124,6 @@ public class PwUpdateFrame extends JFrame {
 		});
 		btnNewButton_1.setBounds(207, 183, 97, 23);
 		getContentPane().add(btnNewButton_1);
-
 
 		this.setSize(400, 300);
 		this.setLocationRelativeTo(null);
