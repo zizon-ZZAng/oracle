f0c37bd70ad0c7b0f6400fc61f4233b037c5078d
yeofriendship <yeofriendship@naver.com>
2023-03-09 15:08:14 +0900

    Update RecommendFrame.java

--- src/frame/RecommendFrame.java
+++ src/frame/RecommendFrame.java
@@ -15,12 +15,18 @@ import service.RecommendServiceImpl;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class RecommendFrame extends JFrame {
 	RecommendService rservice = new RecommendServiceImpl();
 
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	public RecommendFrame() {
 		setTitle("옷 추천");
@@ -36,16 +42,20 @@ public class RecommendFrame extends JFrame {
 		ImageIcon imaget = new ImageIcon(imagecodet);
 
 		Image imagetop = imaget.getImage();
-		Image updateImgt = imagetop.getScaledInstance(500, 500, Image.SCALE_SMOOTH);
+		Image updateImgt = imagetop.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
 
 		ImageIcon updatetop = new ImageIcon(updateImgt);
 		JLabel lblNewLabel_1_1 = new JLabel(updatetop);
 
 		JLabel lblNewLabel_1 = new JLabel("상의");
+		lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 20));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 
 		// --------------------------------------------
 
 		JLabel lblNewLabel_2 = new JLabel("하의");
+		lblNewLabel_2.setFont(new Font("굴림", Font.BOLD, 20));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 
 		// 하의
 		Map<String, Object> retb = rservice.clothesRecommendBottom(map);
@@ -54,7 +64,7 @@ public class RecommendFrame extends JFrame {
 		ImageIcon imageb = new ImageIcon(imagecodeb);
 
 		Image imagebot = imageb.getImage();
-		Image updateImgb = imagebot.getScaledInstance(500, 500, Image.SCALE_SMOOTH);
+		Image updateImgb = imagebot.getScaledInstance(250, 300, Image.SCALE_SMOOTH);
 
 		ImageIcon updatebot = new ImageIcon(updateImgb);
 		JLabel lblNewLabel_2_1 = new JLabel(updatebot);
@@ -62,6 +72,8 @@ public class RecommendFrame extends JFrame {
 		// --------------------------------------------
 
 		JLabel lblNewLabel_3 = new JLabel("신발");
+		lblNewLabel_3.setFont(new Font("굴림", Font.BOLD, 20));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 
 		// 신발
 		Map<String, Object> rets = rservice.clothesRecommendShoes(map);
@@ -70,30 +82,60 @@ public class RecommendFrame extends JFrame {
 		ImageIcon images = new ImageIcon(imagecodes);
 
 		Image imagesho = images.getImage();
-		Image updateImgs = imagesho.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+		Image updateImgs = imagesho.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
 
 		ImageIcon updatesho = new ImageIcon(updateImgs);
 		JLabel lblNewLabel_3_1 = new JLabel(updatesho);
+		
+		// --------------------------------------------
+		
+		textField = new JTextField(rett.get("CLONAME").toString());
+		textField.setEditable(false);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField(retb.get("CLONAME").toString());
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField(rets.get("CLONAME").toString());
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField(rett.get("PRICE").toString());
+		textField_3.setEditable(false);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField(retb.get("PRICE").toString());
+		textField_4.setEditable(false);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField(rets.get("PRICE").toString());
+		textField_5.setEditable(false);
+		textField_5.setColumns(10);
+		
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(12)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(20)
-							.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE)
-							.addGap(74)
-							.addComponent(lblNewLabel_2_1, GroupLayout.PREFERRED_SIZE, 259, GroupLayout.PREFERRED_SIZE)
-							.addGap(64)
-							.addComponent(lblNewLabel_3_1, GroupLayout.PREFERRED_SIZE, 250, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
-							.addGap(322)
-							.addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
-							.addGap(240)
-							.addComponent(lblNewLabel_3, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)))
-					.addGap(131))
+					.addGap(32)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblNewLabel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_1_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 312, Short.MAX_VALUE)
+						.addComponent(textField)
+						.addComponent(textField_3))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblNewLabel_2, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_2_1, GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE)
+						.addComponent(textField_1)
+						.addComponent(textField_4))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblNewLabel_3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_3_1, GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)
+						.addComponent(textField_2)
+						.addComponent(textField_5))
+					.addContainerGap(81, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -103,15 +145,22 @@ public class RecommendFrame extends JFrame {
 						.addComponent(lblNewLabel_1)
 						.addComponent(lblNewLabel_2)
 						.addComponent(lblNewLabel_3))
+					.addGap(42)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblNewLabel_3_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_2_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_1_1, GroupLayout.DEFAULT_SIZE, 335, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(53)
-							.addComponent(lblNewLabel_1_1, GroupLayout.PREFERRED_SIZE, 274, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(69)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_3_1, GroupLayout.PREFERRED_SIZE, 274, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblNewLabel_2_1, GroupLayout.PREFERRED_SIZE, 274, GroupLayout.PREFERRED_SIZE)))))
+						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE))
+					.addGap(13)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(243, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
 
