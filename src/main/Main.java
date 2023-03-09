9f7323f83dfba1dbc92b2a4663970025002074a7
0hzL <123910814+0hzL@users.noreply.github.com>
2023-03-09 21:05:56 +0900

    1

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,15 +9,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		new SignUpFrame();
+		new SignUpFrame();
 
-//		new SignUpFrame();
 		
 //		new MyPageFrame();
 
 //		new WeatherFrame();
 		
-		new LoginFrame();
+//		new LoginFrame();
 		
 //		new RecommendFrame();
 		
