c53854bba6434d32aa8204035eec5aeef68101d7
탁승은 <xkrtmddms@gmail.com>
2023-03-09 18:30:50 +0900

    Update Recommend.java

--- src/dto/Recommend.java
+++ src/dto/Recommend.java
@@ -5,20 +5,27 @@ import lombok.Data;
 @Data
 public class Recommend {
 	
+	
+	//member
 	private String id;
 	private String name;
 	private String gender;
 	private String address;
+	
+	//clothes
 	private long clono;
 	private String cloname;
 	private String thickness;
 	private String catetype;
+	
+	//weather
 	private long code;
 	private String wdate;
 	private int temperature;
 	private int rain;
 	private String locname;
 	
+	//추가
 	private int rank;
 
 }
